fd93705d0cbd06fe69fc8bcd74aecf967478a790
maanas sajeev <154887001+maanas-sajeev@users.noreply.github.com>
2024-01-29 09:27:46 +0530

    Add files via upload

---
+++ cie-see-package/CIE/internals.java
@@ -0,0 +1,11 @@
+package CIE;
+
+public class internals extends student
+{
+public double imarks[] ;
+public internals(String u, String n, int s ,double m[])
+{
+super(u,n,s);
+this.imarks = m;
+}
+}
---
+++ cie-see-package/SEE/externals.java
@@ -0,0 +1,10 @@
+package SEE;
+import CIE.student;
+public class externals extends CIE.student{
+public double smarks[];
+public externals (String u, String n, int s ,double m[])
+{
+super(u,n,s);
+this.smarks = m;
+}
+}
